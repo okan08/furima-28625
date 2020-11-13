791bf93da8eb351338dfa165a74a52a828ec587b
okan08 <okan08@users.noreply.github.com>
2020-11-14 00:32:20 +0900

    本人情報確認バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, inclusion: { in: ["@"] }
   validates :encrypted_password, presence: true, length: { minimum: 6 }, confirmation: true, format: { with: hankaku_eisuji }
-
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z } # 全角(漢字・ひらがな・カタカナ)での入力指定
+  validates :furigana_last, :furigana_first, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # 全角カタカナ指定
+  validates :birthday, presence: true
   
 end
