ab15eb057da8f7f6a9f533678d27ba1a021dc690
okan08 <okan08@users.noreply.github.com>
2020-11-14 00:06:17 +0900

    バリデーションの記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  hankaku_eisuji = /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, inclusion: { in: ["@"] }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, confirmation: true, format: { with: hankaku_eisuji }
+
+  
 end
