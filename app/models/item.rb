4afdb2fbeb78bcaddef0a627c1c87d934b870548
okan08 <okan08@users.noreply.github.com>
2020-11-21 18:51:51 +0900

    バリデーション 設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
+  belongs_to :condition
+  belongs_to :day
+  belongs_to :deliverypay
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :deliverypay_id
+    validates :prefecture_id
+    validates :day_id
+  end
+
+  with_options format: { with: /\A[0-9]+\z/ } do  # 半角数字指定
+    validates :price
+  end
+
+  validates :price, numericality:{ greater_than: 299, less_than: 10000000 }
+
 end
