462bbf1c7f0ba9af5cd9df72e203b087bff7a9e4
okan08 <okan08@users.noreply.github.com>
2020-12-05 17:18:28 +0900

    javascript販売価格の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  
   # get 'items', to: 'items#index'
-  # resources :items, only: [:index, :new, :create]
-
   resources :items
+
+  #get 
 end
