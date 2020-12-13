7109bf3b28c9e6c83b989f72ea6eebed8c1ff669
okan08 <okan08@users.noreply.github.com>
2020-12-13 20:53:53 +0900

    修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,8 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id == @item.user_id
-    else
+    if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
